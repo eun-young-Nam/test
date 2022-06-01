ef2bb3de3a549d5204e2a8278e6918faf0575e74
test <nejlove119@naver.com>
2022-06-01 21:42:14 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-container class="pt-10">
+      <!-- 검색바 -->
+      <search-bar/>
+      <!-- 영화바 -->
+      <movie-list/>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import SearchBar from '@/components/SearchBar'
+import MovieList from '@/components/MovieList'
+
+export default {
+  name: 'App',
+  components: { 
+    SearchBar,
+    MovieList 
+  }
+}
+</script>
